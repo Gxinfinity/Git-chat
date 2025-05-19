43b9bd6307cbaf9ce98c30345ca6efa1245fca23
Gxinfinity <190551265+Gxinfinity@users.noreply.github.com>
2025-05-20 00:40:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 API_ID = "24313439"
 API_HASH = "a007ea183e9276803caba61ad093273b"
-BOT_TOKEN = getenv("BOT_TOKEN", "7718680866:AAEpiQUPUDork9_CbHp56AJtJF_kttU6DQM")
+BOT_TOKEN = getenv("BOT_TOKEN", "7718680866:AAFec68TyBlZbEiskVelXvou5ndsAlvmOWE")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://botbaby:botbaby@cluster0.zngip1b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 OWNER_ID = "7290350162"
 SUPPORT_GRP = "infinitygx_bot_support"
